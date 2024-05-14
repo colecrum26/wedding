c54c4f93ed46c04ac2a6be8591dc726ab23a6dab
colecrum26 <cwcrumrine@gmail.com>
2024-05-14 16:44:56 -0500

    more updates to various pages

--- wedding-website/src/pages/wparty.jsx
+++ wedding-website/src/pages/wparty.jsx
@@ -1,11 +1,42 @@
 import Header from "../components/header";
 
 export default function WeddingParty() {
-    return (
-        <div>
-            <Header />
-            <h1>HELLO</h1>
-            <h3>Our cool friends in the wedding party will be on this page!</h3>
+  return (
+    <div className="page-container">
+      <Header />
+      <div className="content-wrapper wp">
+        <div className="bp-list">
+          <ul className="maids">
+            <li className="bp-item">MARY CONNOR COX</li>
+            <li className="bp-item">HALEY HUGHES</li>
+            <li className="bp-item">TORI CARTER</li>
+            <li className="bp-item">LOGAN COLE</li>
+            <li className="bp-item">REID COX</li>
+            <li className="bp-item">EMMA DONALDSON</li>
+            <li className="bp-item">ROE FERS</li>
+            <li className="bp-item">GIN JAGER</li>
+            <li className="bp-item">MK KELLEY</li>
+            <li className="bp-item">SHELBY LAMON</li>
+            <li className="bp-item">ISABEL YARBOROUGH</li>
+          </ul>
         </div>
-    );
+        <div className="bp-list">
+          <ul className="men">
+            <li className="bp-item">SCOTT CRUMRINE</li>
+            <li className="bp-item">DAVID ANDERSON</li>
+            <li className="bp-item">REID CARTER</li>
+            <li className="bp-item">DENTON COKER</li>
+            <li className="bp-item">DREW CRUMRINE</li>
+            <li className="bp-item">CHASE ELLIOTT</li>
+            <li className="bp-item">CALEB GILBERT</li>
+            <li className="bp-item">JACK HUTCHESON</li>
+            <li className="bp-item">AUSTIN MIMS</li>
+            <li className="bp-item">BEN NORWOOD, IV</li>
+            <li className="bp-item">CHANCE PRUITT</li>
+            <li className="bp-item">TANNER WINGER</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
