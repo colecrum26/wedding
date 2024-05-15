92877202854b36a073028429ea1abaf73036d83f
colecrum26 <cwcrumrine@gmail.com>
2024-05-15 14:39:21 -0500

    updates to schedule formatting

--- wedding-website/src/pages/schedule.jsx
+++ wedding-website/src/pages/schedule.jsx
@@ -7,26 +7,27 @@ export default function Schedule() {
       <div className="content-wrapper">
         <div className="events">
           <h1 className="header-main">FRIDAY, MAY 2, 2025</h1>
-          <h1>Rehearsal Dinner</h1>
-          <p>6:00pm - 8:00pm</p>
+          <h1 className="event-title">Rehearsal Dinner</h1>
+          <p id="io-note">(Invitation Only)</p>
+          <p className="event-time">6:00pm - 8:00pm</p>
           <p>Hans & Franz Biergarten</p>
           <a href="https://maps.app.goo.gl/u9faBurUW4bbZvHh6" className="address-link">3124 South Carolina 14, Greenvile, SC 29615</a>
-          <p>Attire: Casual Cocktail</p>
-          <h1>Welcome Party</h1>
-          <p>8:00pm - 10:00pm</p>
+          <p className="attire">Attire: Casual Cocktail</p>
+          <h1 className="event-title">Welcome Party</h1>
+          <p className="event-time">8:00pm - 10:00pm</p>
           <p>The Captain at Hotel Hartness</p>
           <a href="https://maps.app.goo.gl/DXcL6Mjs8DtGCsM19" className="address-link">120 Halston Avenue, Greenville, SC 29615</a>
-          <p>Attire: Casual Cocktail</p>
+          <p className="attire">Attire: Casual Cocktail</p>
         </div>
         <div className="events">
           <h1 className="header-main">SATURDAY, MAY 3, 2025</h1>
-          <h1>Ceremony & Reception</h1>
-          <p>6:00pm - 11:00pm</p>
+          <h1 className="event-title">Ceremony & Reception</h1>
+          <p className="event-time">6:00pm - 11:00pm</p>
           <p>Hotel Hartness</p>
           <a href="https://maps.app.goo.gl/DXcL6Mjs8DtGCsM19" className="address-link">120 Halston Avenue, Greenville, SC 29615</a>
-          <p>Attire: Black Tie</p>
-          <h1>After Party</h1>
-          <p>11:00pm - 12:30am</p>
+          <p className="attire">Attire: Black Tie</p>
+          <h1 className="event-title">After Party</h1>
+          <p className="event-time">11:00pm - 12:30am</p>
           <p>The Captain at Hotel Hartness</p>
           <a href="https://maps.app.goo.gl/DXcL6Mjs8DtGCsM19" className="address-link">120 Halston Avenue, Greenville, SC 29615</a>
         </div>
--- wedding-website/src/pages/ttd.jsx
+++ wedding-website/src/pages/ttd.jsx
@@ -46,7 +46,11 @@ export default function ThingsToDo() {
         <div className="activities">
           <h1 className="header-main">SPA H</h1>
           <p>90 Traverse Drive, Greenville, SC 29615</p>
-          <p>Book a relaxing facial or massage at the hotel spa!</p>
+          <p>
+            Stretch out and feel your tensions ease at Spa H as expert
+            therapists provide soothing massages and revitalizing facials. Ideal
+            for pre-wedding pampering, romantic weekends, group visits and more.
+          </p>
           <NavLink
             className="faux"
             to="https://www.hotelhartness.com/experiences/spa-h"
@@ -58,7 +62,17 @@ export default function ThingsToDo() {
         <div className="activities">
           <h1 className="header-main">TOPGOLF</h1>
           <p>201 Clifton Court, Greenville, SC 29615</p>
-          <NavLink className="faux" to="https://topgolf.com/us/greenville/" target="_blank">View</NavLink>
+          <p>
+            Enjoy the sprawling entertainment venue with a high-tech driving
+            range and swanky lounge with drinks and games.
+          </p>
+          <NavLink
+            className="faux"
+            to="https://topgolf.com/us/greenville/"
+            target="_blank"
+          >
+            View
+          </NavLink>
         </div>
         <div className="activities">
           <h1 className="header-main">BMW PERFORMANCE CENTER</h1>
@@ -69,13 +83,19 @@ export default function ThingsToDo() {
             Hartness! We promise you won't regret booking this thrilling
             experience.
           </p>
-          <NavLink className="faux" to="https://bmwperformancecenter.com/" target="_blank">View</NavLink>
+          <NavLink
+            className="faux"
+            to="https://bmwperformancecenter.com/"
+            target="_blank"
+          >
+            View
+          </NavLink>
         </div>
         <div className="activities">
           <h1 className="header-main">DOWNTOWN GREENVILLE</h1>
           <p>Greenville, SC 29601</p>
           <p>
-            From walking through Falls Park, to shopping on Main Street, you
+            From walking through Falls Park to shopping on Main Street, you
             will find no shortage of things to do in Downtown Greenville. Make
             sure to take a picture on Liberty Bridge!
           </p>
