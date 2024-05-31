412d154294bc9ffaf7213e445245337e9a521988
colecrum26 <cwcrumrine@gmail.com>
2024-05-30 21:41:07 -0500

    ws registry link

--- wedding-website/src/pages/registry.jsx
+++ wedding-website/src/pages/registry.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Header from "../components/header";
 import RSVPButton from "../components/rsvpBtn";
 
@@ -7,6 +8,13 @@ export default function Registry() {
       <Header />
       <div className="content-wrapper">
         <h3>We'll post our registry here as soon as we finish building it!</h3>
+        <NavLink
+          to="/https://www.williams-sonoma.com/registry/jp2xlwtlrt/registry-list.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Williams Sonoma
+        </NavLink>
       </div>
       <RSVPButton />
     </div>
