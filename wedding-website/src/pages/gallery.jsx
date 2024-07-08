8206c680272239111c992d7e2fa834c6227db905
colecrum26 <cwcrumrine@gmail.com>
2024-07-08 18:05:33 -0500

    added gallery photos

--- wedding-website/src/pages/gallery.jsx
+++ wedding-website/src/pages/gallery.jsx
@@ -1,12 +1,41 @@
 import Header from "../components/header";
 import RSVPButton from "../components/rsvpBtn";
+import shot1 from "../assets/IMG_3996.JPG";
+import shot2 from "../assets/IMG_3997.JPG";
+import shot3 from "../assets/IMG_4007.JPG";
+import shot4 from "../assets/IMG_4010.JPG";
+import shot5 from "../assets/IMG_4022.JPG";
+import shot6 from "../assets/IMG_4034.JPG";
+import shot7 from "../assets/IMG_4047.JPG";
+import shot8 from "../assets/IMG_4051.JPG";
+import shot9 from "../assets/IMG_4064.JPG";
+import shot10 from "../assets/IMG_4078.JPG";
+import shot11 from "../assets/IMG_4087.JPG";
+import shot12 from "../assets/IMG_4100.JPG";
+import shot13 from "../assets/IMG_4108.JPG";
+import shot14 from "../assets/IMG_4109.JPG";
+import shot15 from "../assets/IMG_4111.JPG";
 
 export default function Gallery() {
   return (
     <div className="page-container">
       <Header />
-      <div className="content-wrapper">
-        <h3>Once we take our engagement photos, we'll put them here!</h3>
+      <div className="content-wrapper gallery">
+        <img src={shot1} alt=""  className="gallery-image"/>
+        <img src={shot2} alt=""  className="gallery-image"/>
+        <img src={shot3} alt=""  className="gallery-image"/>
+        <img src={shot4} alt=""  className="gallery-image"/>
+        <img src={shot5} alt=""  className="gallery-image"/>
+        <img src={shot6} alt=""  className="gallery-image"/>
+        <img src={shot7} alt=""  className="gallery-image"/>
+        <img src={shot8} alt=""  className="gallery-image"/>
+        <img src={shot9} alt=""  className="gallery-image"/>
+        <img src={shot10} alt="" className="gallery-image" />
+        <img src={shot11} alt="" className="gallery-image" />
+        <img src={shot12} alt="" className="gallery-image" />
+        <img src={shot13} alt="" className="gallery-image" />
+        <img src={shot14} alt="" className="gallery-image" />
+        <img src={shot15} alt="" className="gallery-image" />
       </div>
       <RSVPButton />
     </div>
--- wedding-website/src/pages/home.jsx
+++ wedding-website/src/pages/home.jsx
@@ -2,13 +2,15 @@ import Header from "../components/header";
 import RSVPButton from "../components/rsvpBtn";
 import Countdown from "../components/countdown";
 import { NavLink } from "react-router-dom";
+import bestLook from "../assets/IMG_4135.JPG"
 
 export default function Home() {
   return (
     <div className="page-container">
       <Header />
+        <h1>May 3, 2025</h1>
       <div className="content-wrapper">
-        {/* <img src="" alt="our best look" /> */}
+        <img src={bestLook} alt="our best look" id="" />
         <div className="info-container">
           <div className="general-info">
             <h1 className="header-main">REBECCA NORWOOD</h1>
@@ -16,7 +18,6 @@ export default function Home() {
             <h1 className="header-main">COLE CRUMRINE</h1>
           </div>
           <div className="rsvp-container">
-            <h2>May 3, 2025</h2>
             <Countdown />
             <h3>Hotel Hartness, Greenville, South Carolina</h3>
             <NavLink className="faux" to="/rsvp">
